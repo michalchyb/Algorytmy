bad243d53e0ebfdac56455cf4086c9caa044d748
Michał Chyb <michalchyb1@gmail.com>
2019-07-29 22:01:11 +0200

    added ArrayToBinary refactor

--- src/main/java/pl/mchyb/algorytmy/ArrayToBinar.java
+++ src/main/java/pl/mchyb/algorytmy/ArrayToBinar.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class ArrayToBinar {
     public static int ConvertBinaryArrayToInt(List<Integer> binary) {
 
-
+       String concat = "";
+        for(Integer i : binary) {
+            concat += Integer.toString(i);
+        }
+        return Integer.parseInt(concat,2);
     }
 }
